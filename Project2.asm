9cbb9d9d5259722dbea1d423fdbb803e0c6ab6c2
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2018-11-04 17:07:48 -0500

    Created string to print. Fixed register mistake.

--- Project2.asm
+++ Project2.asm
@@ -1,11 +1,11 @@
 .data
 
+message: .asciiz "Hi user! Type something." #The code should print this string out.
 
 .text
 
 main: 
 
-li $vo, 10
+li $v0, 10
 syscall
 #Basically indicates the end of 'main'.
-
