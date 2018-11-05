a5fe604d9a9925f02738c4894a684a38dfa489bc
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2018-11-05 12:35:32 -0500

    Added new functions.
    Added new function so that the user can input text.

--- Project2.asm
+++ Project2.asm
@@ -1,11 +1,18 @@
 .data
 
 message: .asciiz "Hi user! Type something." #The code should print this string out.
-
+userInput: .space 20 #Reserves 20 bytes of data for whatever the person types in
 .text
 
 main: 
+li $v0, 8  #Tells the system to prepare to read the text from the user
+la $a0, userInput #Stores the user's input into the register
+li $a1, 20 #Tells the system the maximum length of the text
+syscall #Exceutes the function
+
+
 
 li $v0, 10
 syscall
 #Basically indicates the end of 'main'.
+
