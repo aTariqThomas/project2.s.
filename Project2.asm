500fc958df9edc6858edfc5fe39784501edb2815
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2018-11-16 16:55:21 -0500

    Started Project again
    Restarted the project because i wasn't satisfied with how I had started out.

--- Project2.asm
+++ Project2.asm
@@ -1,26 +1,19 @@
+#Tariq Thomas 
+#Student ID is @02845739
+
+# My N is 36 (N = 36)
+
+
+
+
+
+
 .data
 
-message: .asciiz "Hi user! Type something: " #The code should print this string out.
-userInput: .space 20 #Reserves 20 bytes of data for whatever the person types in
-.text
-
-main: 
-li $v0, 8  #Tells the system to prepare to read the text from the user
-la $a0, userInput #Stores the user's input into the register
-li $a1, 20 #Tells the system the maximum length of the text
-syscall #Exceutes the function
-
-
-li $v0, 4 #Code to display text
-la $a0, message
-syscall #Executs the call to display the text in the 'message' label
-
-
-#Displays text from User
-li $v0, 4
-la $a0, userInput
-syscall
-
-li $v0, 10
-syscall
-#Basically indicates the end of 'main'.
+  long_Inpt:    .asciiz "Input is too long."
+  
+  empty_Inpt:   .asciiz "Input is empty."
+  
+  user_Inpt:    .space  500
+  
+  invalid_Inpt: .asciiz "Invalid base-N number." # change n
