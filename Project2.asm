ff76a08fd7dcc2b9bf24357e06478fa09ae2e12a
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2018-11-16 22:45:52 -0500

    Created third digit function

--- Project2.asm
+++ Project2.asm
@@ -233,4 +233,10 @@ exit:
 	addi $s0, $s0, -1
 	addi $a0, $a0, 1
 	j base_convert_input #jump to this function
-
+	
+	thrdDigit:
+	#The next line basically does (base N)^1
+	li $s6, 36
+	mult $s4, $s6
+	mflo $s7
+	add $t7, $t7, $s7
